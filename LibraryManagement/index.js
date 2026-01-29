eaca7559a39cedf084fa0fc3367e8b088a5b959f
codeblade123 <rt907075@gmail.com>
2026-01-29 11:14:36 +0530

    [Minesh] add files

---
+++ LibraryManagement/index.js
@@ -0,0 +1,24 @@
+/*import Book from "./Book.js";
+let result=Book(
+    "121",
+    "Javascript",
+    "abc",
+    "300"
+
+);*/
+
+/*import member from "./Member.js";
+let result=member(
+    "1",
+    "kaustubhi",
+    "gold"
+
+);*/
+
+import book from './Book.js'
+import member from './Member.js'
+import borrow from './borrow.js';
+let c=borrow(1,121);
+console.log(c);
+
+
